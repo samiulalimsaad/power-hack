c085977e3e816ccb0f9b8a3e72880d6ca3ca0bf8
samiulalimsaad <saadnbiuraj@gmail.com>
2022-06-30 23:40:27 +0600

    searchbar added

---
+++ src/Components/Table/SearchBar.tsx
@@ -0,0 +1,23 @@
+const SearchBar = () => {
+    return (
+        <div className="py-4 my-10 bg-slate-400 sm:px-20">
+            <div className="flex justify-between">
+                <div className="flex">
+                    <a className="text-xl normal-case btn btn-ghost">Bolling</a>
+                    <div className="form-control">
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            className="input input-bordered"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <button className="btn btn-primary">Add New Bill</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SearchBar;
